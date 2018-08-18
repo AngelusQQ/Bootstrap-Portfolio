66eb05c773936d4b665440f88adb02afb4645bb7
William <william.ma9215@gmail.com>
2018-08-18 12:49:44 -0400

    Linked portfolio page to other HW's

---
+++ assets/javascript/js.js
@@ -0,0 +1,19 @@
+$('#b1').on("click", function() {
+  window.location.href = "https://angelusqq.github.io/Word-Guess-Game/";
+});
+
+$('#b2').on("click", function() {
+  window.location.href = "https://angelusqq.github.io/unit-4-game/";
+});
+
+$('#b3').on("click", function() {
+  window.location.href = "https://angelusqq.github.io/TriviaGame/";
+});
+
+$('#b4').on("click", function() {
+  window.location.href = "https://angelusqq.github.io/giphyHW/";
+});
+
+$('#b5').on("click", function() {
+  window.location.href = "https://angelusqq.github.io/RPS-Multiplayer/";
+});
